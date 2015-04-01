178607b324c8166017bc3f21a79eab4fe47fe017
elcalango <josereato@outlook.com>
2015-04-01 20:46:58 -0300

    aula 18

---
+++ Quiron.LojaVirtual.UnitTest/UnitTest2.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class TesteCarrinhoCompras
+    {
+        /// <summary>
+        /// Teste adicionar itens ao carrinho
+        /// </summary>
+        [TestMethod]
+        public void AdicionarItensAoCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 2);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto3, 3);
+            //Act
+            ItemCarrinho[] itens = carrinho.ItemCarrinhos.ToArray();
+
+            //Assert
+            Assert.AreEqual(itens.Length, 3);
+            Assert.AreEqual(itens[0].Produto, produto1);
+            Assert.AreEqual(itens[1].Produto, produto2);
+        }
+
+        [TestMethod]
+        public void AdicionarProdutoExistenteParaCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+            //Produto produto3 = new Produto
+            //{
+            //    ProdutoId = 3,
+            //    Nome = "Teste 3"
+            //};
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 10);
+
+            //Act
+            ItemCarrinho[] resultado = carrinho.ItemCarrinhos
+                .OrderBy(c => c.Produto.ProdutoId).ToArray();
+
+            Assert.AreEqual(resultado.Length, 2);
+
+            Assert.AreEqual(resultado[0].Quantidade, 11);
+        }
+
+        [TestMethod]
+        public void RemoverItensCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto3, 5);
+            carrinho.AdicionarItem(produto2, 1);
+
+            //Act
+            carrinho.RemoverItem(produto2);
+
+            Assert.AreEqual(carrinho.ItemCarrinhos
+                .Where(c => c.Produto == produto2)
+                .Count(), 0);
+        }
+
+        [TestMethod]
+        public void CalcularValorTotal()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 3);
+
+            decimal resultado = carrinho.ObterValorTotal();
+
+            Assert.AreEqual(resultado, 450M);
+
+        }
+
+        [TestMethod]
+        public void LimparItensCarrinho()
+        {
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+
+            carrinho.LimparCarrinho();
+
+            Assert.AreEqual(carrinho.ItemCarrinhos.Count(), 0);
+        }
+    }
+}
--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -4,35 +4,78 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Quiron.LojaVirtual.Web.Controllers;
 using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.Web
 {
     public class RouteConfig
     {
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+        //    // 1 - Home
+
+        //    routes.MapRoute(null, "", new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null, pagina = 1 });
+
+
+
+        //    // 2 - 
+        //    routes.MapRoute(null,
+        //        "Pagina{pagina}",
+        //        new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null }, new { pagina = @"\d+" });
+
+
+        //    routes.MapRoute(null,
+        //        "{categoria}", new { controller = "Vitrine", action = "ListaProdutos", pagina = 1 });
+
+
+
+        //    routes.MapRoute(null,
+        //        "{categoria}/Pagina{pagina}", new { controller = "Vitrine", action = "ListaProdutos" }, new { pagina = @"\d+" });
+
+
+
+        //    routes.MapRoute(null, "{controller}/{action}");
+
+
+        //}
         public static void RegisterRoutes(RouteCollection routes)
         {
             //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // 1 - sem parametros
-            routes.MapRoute(null,
-                "",
-                new { Controller = "Vitrine", Action = "ListaProdutos", categoria = (string)null, pagina = 1 });
-            //2 somente a Pagina1, Pagina2, PaginaN
+            //1 - sem parametros
+            routes.MapRoute(name: null,
+                url: "",
+                defaults: new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null, pagina = 1 });
+
+            
+
+
+            ////2 somente a Pagina1, Pagina2, PaginaN
             routes.MapRoute(null,
                 "Pagina{pagina}",
-                new { Controller = "Vitrine", Action = "ListaProdutos", categoria = (string)null }, new { pagina = @"\d+" });
-            //3 somente a categoria
-            routes.MapRoute(null, "categoria",
-                new { controller = "Vitrine", action = "ListaProdutos", pagina = 1 });
-            //4 
+                new { controller = "Vitrine", action = "ListaProdutos", categoria = (string)null }, new { pagina = @"\d+" });
+            ////3 somente a categoria
+            routes.MapRoute(null, "{categoria}",
+                new { controller = "Vitrine", action = "ListaProdutos", categoria = UrlParameter.Optional });
+            ////4 
             routes.MapRoute(null,
                 "{categoria}Pagina{pagina}",
-                new { Controller = "Vitrine", Action = "ListaProdutos" }, new { pagina = @"\d+" });
+                new { controller = "Vitrine", action = "ListaProdutos" }, new { pagina = @"\d+" });
 
             //default
-            routes.MapRoute(null,
-                "{controller}/{action}");
+            //routes.MapRoute(null,
+                //"{controller}/{action}");
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Vitrine", action = "ListaProdutos",  id = UrlParameter.Optional }
+            );
+
+
 
         }
 
