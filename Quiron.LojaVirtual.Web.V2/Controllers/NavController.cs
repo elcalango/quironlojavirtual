4f95ed4ab5455ae39a948efc5e64750f7a22e8ac
José Reato Neto <josereato@outlook.com>
2016-03-12 17:20:50 -0300

    aula 61

---
+++ Quiron.LojaVirtual.Web.V2/Controllers/NavController.cs
@@ -0,0 +1,26 @@
+﻿using Quiron.LojaVirtual.Web.V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.V2.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("nav/{id}/{marca}")]
+        public ActionResult ObterProdutosPorMarcas(string id)
+        {
+            var _model = new ProdutosViewModel { Produtos = null };
+
+            return View("Index", _model);
+        }
+    }
+}
